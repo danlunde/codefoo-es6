612b0868755b7a380cd34bc6ed10600ec99c625a
Daniel Lunde <danlunde@gmail.com>
2018-07-06 00:47:40 -0400

    default spectacle app

---
+++ src/presentation.js
@@ -0,0 +1,130 @@
+// Import React
+import React from 'react'
+
+// Import Spectacle Core tags
+import { BlockQuote, Cite, Deck, Heading, ListItem, List, Quote, Slide, Text, Code } from 'spectacle'
+
+// Import theme
+import createTheme from 'spectacle/lib/themes/default'
+
+import CodeSlide from 'spectacle-code-slide'
+// import shiaLabeoufMagicGif from "./shiaLabeoufMagic.gif";
+// import preloader from "spectacle/lib/utils/preloader";
+
+// preloader({
+//   shiaLabeoufMagicGif
+// });
+
+// Require code snippets
+/* eslint import/no-webpack-loader-syntax: off */
+
+// Require CSS
+require('normalize.css')
+
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quartenary: '#CECECE',
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica',
+  }
+)
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
+        <Slide transition={['zoom']} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Spectacle Boilerplate
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            open the presentation/index.js file to get started
+          </Text>
+        </Slide>
+        <Slide>
+          <Heading>I ❤️ ES6</Heading>
+          <Text>or ... how I can tolerate writing JavaScript again</Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>
+            Typography
+          </Heading>
+          <Heading size={1} textColor="secondary">
+            Heading 1
+          </Heading>
+          <Heading size={2} textColor="secondary">
+            Heading 2
+          </Heading>
+          <Heading size={3} textColor="secondary">
+            Heading 3
+          </Heading>
+          <Heading size={4} textColor="secondary">
+            Heading 4
+          </Heading>
+          <Heading size={5} textColor="secondary">
+            Heading 5
+          </Heading>
+          <Text size={6} textColor="secondary">
+            Standard text
+          </Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Standard List
+          </Heading>
+          <List>
+            <ListItem>Item 1</ListItem>
+            <ListItem>Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+            <ListItem>Item 4</ListItem>
+          </List>
+        </Slide>
+        <CodeSlide
+          transition={[]}
+          lang="js"
+          code={require('raw-loader!./snippets/arrow-functions-1.js')}
+          ranges={
+            [
+              { loc: [0, 99], title: '() => {}' },
+              { loc: [0, 1], title: 'The Beginning' },
+              { loc: [1, 2] },
+              { loc: [1, 2], note: 'Heres a note!' },
+              { loc: [2, 3] },
+              { loc: [4, 7] },
+              { loc: [8, 10] },
+            ]
+            // ...
+          }
+        />
+        <CodeSlide
+          transition={[]}
+          lang="js"
+          code={require('raw-loader!./snippets/destructuring.example')}
+          ranges={
+            [
+              { loc: [0, 99], title: 'Walking through some code' },
+              { loc: [0, 1], title: 'The Beginning' },
+              { loc: [1, 2] },
+              { loc: [1, 2], note: 'Heres a note!' },
+              { loc: [2, 3] },
+              { loc: [4, 7] },
+              { loc: [8, 10] },
+            ]
+            // ...
+          }
+        />
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Example Quote</Quote>
+            <Cite>Author</Cite>
+          </BlockQuote>
+        </Slide>
+      </Deck>
+    )
+  }
+}
